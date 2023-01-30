2752571f0947a1b24135b9d5bc0ff75c70def386
= <=>
2023-01-29 19:54:42 -0500

    Started the generation of the bars

---
+++ src/components/bars/bar.js
@@ -0,0 +1,11 @@
+import styles from './bar.module.css'
+
+export default function Bar({height}){
+    
+    // let [height, width] = [Math.random() * (max_height - 5) + (5), (100/max_height)]
+    // console.log(`Bar was generated with height: ${height} and width: ${width}`)
+    // return <div className={styles.bar} style={{
+    //     height: `${height}vh`,
+    //     width: `${width}%`
+    // }}></div>    
+}
--- src/containers/visulaizer.js
+++ src/containers/visulaizer.js
@@ -1,10 +1,24 @@
 import styles from './visulaizer.module.css'
+import Bar from '../components/bars/bar'
 
 export default function Visualizor(){
-    function Generate_bar(){
-        
-    }
-    return <div className={styles.container}>
+    
+    // Check if the number of bars if possibility
+    let max_number_bars = (number_bars) => {
+        let res;
 
+        return res
+    }
+
+    return <div className={styles.container}>
+        <div className={styles.config}>
+            <div className={styles.input_container}></div>
+            <div className={styles.icon}>
+                <i class="fa-solid fa-sliders" style={{color: "white"}}></i>
+            </div>
+        </div>
+        <div className={styles.barContainer}>
+            {[...new Array(max_number_bars)].map( (_,index)=> <Bar key={index} number_of_bars={max_number_bars} max_height={87}/>)}
+        </div>
     </div>
 }
